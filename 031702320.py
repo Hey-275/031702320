3b8945107b7aa335b6f6fb1501bba2c1e5c37823
Hey-275 <55265986+Hey-275@users.noreply.github.com>
2019-09-27 16:20:40 +0800

    Update 031702320.py

--- 031702320.py
+++ 031702320.py
@@ -62,11 +62,11 @@ del list2[-1]
 list2 = list2 + list4
 list2 = list2 + list5
 
-if not Match2.__contains__(list2[0]):
-    if list2[-1]=='区'：
-    list2[0]=list2[0][0:-3]
-    else：
-    list2[0]=list2[0][0:-1]
+#if not Match2.__contains__(list2[0]):
+ #   if list2[-1]=='区'：
+  #  list2[0]=list2[0][0:-3]
+  #  else：
+  #  list2[0]=list2[0][0:-1]
 if list2[0] == '上海市'：
     list2[0] = list2[0][0:2]
     list2[1]='上海市'
