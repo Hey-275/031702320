9cea5065636a906ad0fd82b0990ed9ac48202c6f
Hey-275 <55265986+Hey-275@users.noreply.github.com>
2019-09-29 13:54:30 +0800

    Update 031702320.py

--- 031702320.py
+++ 031702320.py
@@ -23,10 +23,10 @@ def getaddr(str):
     str_step1 = Match1.group()
     str_step2 = re.sub('\d{11}|', '', str_step1)
     Match2 = re.sub('\.', '', str_step2)
-    if Match2[0:2]=='吉林':
-        Match2=Match2[2:]
-        if Match2[0]=='省':
-            Match2=Match2[1:]
+    #if Match2[0:2]=='吉林':
+        #Match2=Match2[2:]
+        #if Match2[0]=='省':
+            #Match2=Match2[1:]
     str_step3 = cpca.transform([Match2], cut=False, open_warning=False)  # 调用cpca模块分出地址簿前三级
     list1 = str_step3.values.tolist()
     if (list1[0][0][0:2] == Match2[0:2]):
